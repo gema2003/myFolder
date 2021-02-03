c3582a69b0fb486aaf0226ec9a8caf1561440106
Gema <gema3035@gmail.com>
2021-02-03 17:31:31 -0600

    first commit

---
+++ Java/codigo.js
@@ -0,0 +1,10 @@
+
+function go(){
+
+if (document.form.password.value=='55053' && document.form.login.value=='Invited'){ 
+        document.form.submit(); 
+    } 
+    else{ 
+         alert("Access denied"); 
+    } 
+} 
